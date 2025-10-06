ceff162e2fd1c5dc41542ee46c69266c9c2a3a6d
Antarikh-02 <bantarikh@gmail.com>
2025-10-06 12:50:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hero from "./sections/Hero";
+import AboutMe from "./sections/AboutMe";
+import Projects from "./sections/MyProjects";
+import SkillsEducation from "./sections/SkillsandEducation";
+import Home from "./sections/Home";
+import GetStarted from "./sections/GetStarted";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+
+        <Route path="/" element={<GetStarted />} />
+        {/* Home page */}
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/hero" element={<Hero />} />
+
+        {/* About Me page */}
+        <Route path="/about" element={<AboutMe />} />
+
+        {/* My Projects page */}
+        <Route path="/projects" element={<Projects />} />
+
+        {/* Skills and education page */}
+        <Route path="/skillsedu" element={<SkillsEducation />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
